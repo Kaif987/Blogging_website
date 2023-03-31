c706ab2ecdcd74838f311aea3ff5ec58326b5667
Kaif Siddiqui <mdkaifprofession@gmail.com>
2023-03-31 22:43:03 +0545

    Add React-router-dom

--- bloggin/src/Components/Navbar.tsx
+++ bloggin/src/Components/Navbar.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import {AiFillInstagram} from 'react-icons/ai'
+import {GrFacebook, GrTwitter} from 'react-icons/gr'
+import {CgProfile} from 'react-icons/cg'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <nav className='fixed top-0 left-0 shadow-md py-2 w-full'>
-        <ul className=' flex justify-center gap-6 text-gray-500 text-xl'>
-            <li className='hover:cursor-pointer'>Home</li>
-            <li className='hover:cursor-pointer'>About</li>
-            <li className='hover:cursor-pointer'>Contact</li>
-            <li className='hover:cursor-pointer'>Write</li>
+    <nav className='fixed top-0 left-0 shadow-md py-2 w-full flex justify-around items-center'>
+        <div className='flex gap-3 justify-center items-center'>
+           <AiFillInstagram className='h-full hover:cursor-pointer w-6'/>
+           <GrFacebook className='h-full hover:cursor-pointer w-5'/>
+           <GrTwitter className='h-full hover:cursor-pointer w-5'/>
+        </div>
+        <ul className=' flex justify-center gap-6 text-gray-900 text-xl font-light'>
+            <li className='hover:cursor-pointer'><Link to="/">Home</Link></li>
+            <li className='hover:cursor-pointer'><Link to="/about">About</Link></li>
+            <li className='hover:cursor-pointer'><Link to="/contact">Contact</Link></li>
+            <li className='hover:cursor-pointer'><Link to="/write">Write</Link></li>
             <li className='hover:cursor-pointer'>Logout</li>
         </ul>
+        <div className=''>
+          <CgProfile className='w-7 h-7 hover:cursor-pointer'/>
+        </div>
     </nav>
   )
 }
--- bloggin/src/Pages/Homepage.tsx
+++ bloggin/src/Pages/Homepage.tsx
@@ -4,6 +4,10 @@ export default function Homepage() {
   return (
     <div>
         <Navbar/>
+        <img src="https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80"
+        alt=""
+        className="w-full h-20 object-cover"
+        />
     </div>
   )
 }
